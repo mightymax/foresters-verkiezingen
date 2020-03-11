8b0f22afe07703758f0eaa7fc4a16628afcc2015
Mark Lindeman <m.lindeman@picturae.com>
2020-03-11 09:12:22 +0100

    Admin cmd to fetch all codes

--- public/api/admin.php
+++ public/api/admin.php
@@ -53,6 +53,7 @@ switch ($cmd) {
 		$db = new MyDB();
 		header('Content-Type: application/json');
 		echo json_encode($db->getCodes(@$_GET['code']));
+		break;
 	default:
 		header("HTTP/1.1 405 Method Not Allowed");
 		err("Unkown or illegale remote procedure call.");
